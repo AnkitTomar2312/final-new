ccaa3674c0bafd6574e96591deb7f89e556b02e4
Ankit Tomar <ankitjdec@gmail.com>
2024-02-15 22:35:27 +0530

    trying chat gpt code to test the api working

--- server/index.js
+++ server/index.js
@@ -4,27 +4,26 @@ require("./db/config");
 const User = require("./db/Users");
 const Questions = require("./db/Questions");
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-//testing
+
+// Routes
+// Testing route
 app.post("/test", (req, res) => {
-  res.send({ result: "POst working" });
+  res.send({ result: "POST working" });
 });
-//register API
+
+// Register route
 app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
   console.log(result);
   res.send({ result: "success" });
 });
-//loginapi
+
+// Login route
 app.post("/login", async (req, res) => {
   console.log(req.body);
   if (req.body.password && req.body.email) {
@@ -37,7 +36,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-//question api
+// Question route
 app.get("/array-questions", async (req, res) => {
   try {
     let result = await Questions.find();
@@ -52,4 +51,70 @@ app.get("/array-questions", async (req, res) => {
   }
 });
 
-app.listen(5000);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+//pervious code
+// const express = require("express");
+// const cors = require("cors");
+// require("./db/config");
+// const User = require("./db/Users");
+// const Questions = require("./db/Questions");
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
+// //testing
+// app.post("/test", (req, res) => {
+//   res.send({ result: "POst working" });
+// });
+// //register API
+// app.post("/register", async (req, res) => {
+//   let user = new User(req.body);
+//   let result = await user.save();
+//   console.log(result);
+//   res.send({ result: "success" });
+// });
+// //loginapi
+// app.post("/login", async (req, res) => {
+//   console.log(req.body);
+//   if (req.body.password && req.body.email) {
+//     let result = await User.findOne(req.body).select("-password");
+//     if (result) {
+//       res.send(result);
+//     } else {
+//       res.send({ result: "user not found" });
+//     }
+//   }
+// });
+
+// //question api
+// app.get("/array-questions", async (req, res) => {
+//   try {
+//     let result = await Questions.find();
+//     if (result.length > 0) {
+//       res.send(result);
+//     } else {
+//       res.send({ result: "nothing to show" });
+//     }
+//   } catch (error) {
+//     console.error("Error retrieving questions:", error);
+//     res.status(500).send("Internal server error");
+//   }
+// });
+
+// app.listen(5000);
