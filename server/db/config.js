975d31d76cdfbc29ab7f36c1aafd90a97460d6c4
Ankit Tomar <ankitjdec@gmail.com>
2024-02-15 20:52:38 +0530

    added the mongodb link

--- server/db/config.js
+++ server/db/config.js
@@ -1,2 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/Aupdeshik");
+//mongoose.connect("mongodb://127.0.0.1:27017/Aupdeshik");
+mongoose.connect(
+  "mongodb+srv://ankitjdec:SZOaclcNy5DSNRge@users.rkpgw8h.mongodb.net/?retryWrites=true&w=majority"
+);
